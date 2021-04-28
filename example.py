5d854a6d0b79dd32a85286e28c724f9156878365
ryoh908 <ryoh908>
2021-04-28 20:02:42 +0000

    x

--- example.py
+++ example.py
@@ -1,9 +1,5 @@
 # Python Module example
 
 def add(a, b):
-   """This program adds two
-   numbers and return the result"""
-
-   result = a + b
-   return result
-   
+   return a + b
+  
---
+++ test.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+import sys
+import os
+import example
+
+# COMMAND ----------
+
+example.add(2,3)
+
+# COMMAND ----------
+
+
