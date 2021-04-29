54d9868b77ce70065dc39af0493d1876b7c7308d
ryoh908 <ryoh908>
2021-04-29 01:37:09 +0000

    3rd

--- test.py
+++ test.py
@@ -5,11 +5,41 @@ import example
 
 # COMMAND ----------
 
+import pandas as pd
+import numpy as np
+from pandas_profiling import ProfileReport
+df = pd.DataFrame(
+    np.random.rand(100, 5),
+    columns=['a', 'b', 'c', 'd', 'e']
+)
+
+# COMMAND ----------
+
+df.head(5)
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+profile = ProfileReport(df, title='Pandas Profiling Report')
+
+# COMMAND ----------
+
 example.add(2,3)
 
 # COMMAND ----------
 
 example.multi(2,2.9)
+
+# COMMAND ----------
+
+dbutils.fs.ls("/")
+
+# COMMAND ----------
+
+dbutils.fs.ls("dbfs:/FileStore/tables/titanic/")
 
 # COMMAND ----------
 
